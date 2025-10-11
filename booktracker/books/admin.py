179baad4b37a622933bd703dd5837681f86a7d2a
nafisa9102 <tasnimnafisa09@gmail.com>
2025-10-11 11:42:01 +0600

    Add/Update code for ReadMap project

--- booktracker/books/admin.py
+++ booktracker/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Category, Book, Borrow, Rating
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Borrow)
+admin.site.register(Rating)
